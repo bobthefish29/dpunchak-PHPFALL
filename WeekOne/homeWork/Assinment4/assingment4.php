38ef51c04164d45459a2debc99fff86776cf4f58
bobthefish29 <dapunchak@email.neit.edu>
2024-09-30 10:58:50 -0400

    StartOfAssinment4

---
+++ WeekOne/homeWork/Assinment4/assingment4.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <!-- this is the first assigmet for lists -->
+        <h1>Hello</h1>
+        
+    </body>
+</html>
---
+++ WeekOne/homeWork/Assinment4/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+$animle = ['dog','cat'];
+
+
+function dd($animle){
+
+    echo '<pre>';
+    die(var_dump($animle));
+    echo '</pre>';
+
+}
+
+
+dd($animle);
+
+
+
+// this is the whole webPage
+require 'assingment4.php';
