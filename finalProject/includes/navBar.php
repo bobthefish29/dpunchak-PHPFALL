3a3590852dbd2363104074b1c3bb021151bc6463
bobthefish29 <dapunchak@email.neit.edu>
2024-10-24 16:24:46 -0400

    startFinal

---
+++ finalProject/includes/navBar.php
@@ -0,0 +1,12 @@
+<nav id="navBar">
+    <div id="left"><label>Pet Pet's +</label></div>
+    <div id="middle">
+        <div class="navLink"><a href="#?Home">Home</a></div>
+        <div class="navLink"><a href="#?Goal">Our Goal</a></div>
+        <div class="navLink"><a href="#?contact">Contact</a></div>
+        <div class="navLink"><a href="#?Shot">Shop</a></div>
+        <div class="navLink"><a href="#?app">Make Appointment</a></div>
+    </div>
+    <div id="Right"><a href="#?LogIN">LOG IN</a></div>
+    
+</nav>
---
+++ finalProject/index.php
@@ -0,0 +1,34 @@
+<!-- The way it will look like is 
+
+
+    CONTACT INFO
+    AD
+    MAIN
+        Image of pets
+        Locations
+        Certifations
+    Footer
+        More contact Info
+-->
+<!-- the is for the style addings -->
+<style>
+    <?php include 'style/boilerPage.css';?>
+    <?php include 'style/incluesStyles/navBar.css';?>
+
+    <?php include 'style/pages/homePage.css';?>
+</style>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pet Pet's +</title>
+    </head>
+    <body>
+        <section>Contact info / externial meida links</section>
+        <?php include "includes/navBar.php";?>
+        <artical>ad</artical>
+        <main>main</main>
+        <footer>footer</footer>
+    </body>
+</html>
